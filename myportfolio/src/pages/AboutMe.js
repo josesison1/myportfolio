0b0d03eefb1ba289291236d5923d498800800a28
josesison1 <learnacademy@LEARNs-Air.lan>
2022-05-15 21:49:07 -0700

    updates

--- myportfolio/src/pages/AboutMe.js
+++ myportfolio/src/pages/AboutMe.js
@@ -3,6 +3,7 @@ import aboutmepic from '../img/mypic.jpeg'
 import laptop from '../img/computer3.png'
 import download from '../img/download.png'
 import resume from '../assets/Jose Sison Full Stack Developer Resume.pdf'
+import location from '../img/location.png'
 
 
 class AboutMe extends Component {
@@ -14,11 +15,11 @@ class AboutMe extends Component {
         <div className="aboutme">
           <div className="aboutme1"><img id="myimage" src={aboutmepic} width="350" />
             <p className="myname">Jose Sison</p>
-            <p className="sandiego">San Diego, CA.</p>
+            <p className="sandiego"> <img id='locationicon' src={location} />San Diego, CA.</p>
           </div>
           <div className="aboutmyself">
-            <div className="hello">
-              <img className="laptoppic" src={laptop} width="400" />
+            <div className='hello'>
+              <img className="laptoppic" src={laptop} />
               <p id="aboutmeparagraph">Hello! I'm a full stack developer with experience in various programming languages and frameworks. Eager to hone
                 technical expertise and fulfill my passion for building efficient and functional web applications. I enjoy problem
                 solving, learning new technologies, and finding ways to utilize my skills to solve real world problems through
