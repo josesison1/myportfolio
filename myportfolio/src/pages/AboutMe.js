d3ad1c90f6685b223bbec07b0afe79a0256e2640
josesison <josesison1@yahoo.com>
2022-06-23 16:55:19 -0700

    updated resume

--- myportfolio/src/pages/AboutMe.js
+++ myportfolio/src/pages/AboutMe.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 import aboutmepic from '../img/mypic.jpeg'
 import laptop from '../img/computer3.png'
 import download from '../img/download.png'
-import resume from '../assets/Jose Sison Full Stack Developer.pdf'
+import resume from '../assets/Jose Sison Software Developer Resume.pdf'
 
 class AboutMe extends Component {
   render() {
